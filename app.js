c51c80210dc4171f84823d2401e588742159361f
bboduroglu <burakboduroglu0@gmail.com>
2023-06-02 12:19:02 +0300

    partials folder added.

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const ejs = require('ejs');
 
 const app = express();
 const PORT = 5000;
@@ -9,7 +10,21 @@ app.set('view engine', 'ejs');
 //MIDDLEWARES
 app.use(express.static('public'));
 
-app.get('/', (req, res) => {});
+app.get('/', (req, res) => {
+  res.render('index');
+});
+
+app.get('/about', (req, res) => {
+  res.render('about');
+});
+
+app.get('/post', (req, res) => {
+  res.render('post');
+});
+
+app.get('/add', (req, res) => {
+  res.render('add_post');
+});
 
 app.listen(PORT, () => {
   console.log(`SERVER PORT: ${PORT}`);
