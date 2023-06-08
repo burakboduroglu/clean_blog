b34921810871f068ce93f47a296c27180318e05d
bboduroglu <burakboduroglu0@gmail.com>
2023-06-08 12:08:35 +0300

    mongodb added.

--- app.js
+++ app.js
@@ -1,17 +1,30 @@
 const express = require('express');
 const ejs = require('ejs');
+const Post = require('./models/Post');
+const mongoose = require('mongoose');
 
 const app = express();
 const PORT = 5000;
+
+//DB Connection
+mongoose.connect('mongodb://127.0.0.1:27017/cleanblog-db', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
 //TEMPLATE ENGINE
 app.set('view engine', 'ejs');
 
 //MIDDLEWARES
 app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
-app.get('/', (req, res) => {
-  res.render('index');
+app.get('/', async (req, res) => {
+  const blogs = await Post.find({});
+  res.render('index', {
+    blogs,
+  });
 });
 
 app.get('/about', (req, res) => {
@@ -22,8 +35,13 @@ app.get('/post', (req, res) => {
   res.render('post');
 });
 
-app.get('/add', (req, res) => {
+app.get('/add_post', (req, res) => {
   res.render('add_post');
+});
+
+app.post('/blogs', async (req, res) => {
+  await Post.create(req.body);
+  res.redirect('/');
 });
 
 app.listen(PORT, () => {
